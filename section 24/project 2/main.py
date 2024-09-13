1b5ddeb964a13ae53e7d95572645af1ca32e0018
rezadrian01 <ahmadadrian324@gmail.com>
2024-09-13 10:47:22 +0700

    finishing mail merge project

--- section 24/project 2/main.py
+++ section 24/project 2/main.py
@@ -1,8 +1,11 @@
-#TODO: Create a letter using starting_letter.txt 
-#for each name in invited_names.txt
-#Replace the [name] placeholder with the actual name.
-#Save the letters in the folder "ReadyToSend".
-    
-#Hint1: This method will help you: https://www.w3schools.com/python/ref_file_readlines.asp
-    #Hint2: This method will also help you: https://www.w3schools.com/python/ref_string_replace.asp
-        #Hint3: THis method will help you: https://www.w3schools.com/python/ref_string_strip.asp
+PLACEHOLDER = '[name]'
+with open('section 24/project 2/Input/Names/invited_names.txt') as list_name:
+    names = list_name.readlines()
+
+with open('section 24/project 2/Input/Letters/starting_letter.txt') as letter:
+    letter_form = letter.read()
+    for name in names:
+        formatted_name = name.strip()
+        new_letter = letter_form.replace(PLACEHOLDER, formatted_name)
+        with open(f"section 24/project 2/Output/ReadyToSend/letter_for_{formatted_name}.docx", mode='w') as completed_letter:
+            completed_letter.write(new_letter)
