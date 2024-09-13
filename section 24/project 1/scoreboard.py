323165f0efbc71c3328e1dba392238dbeafd1061
rezadrian01 <ahmadadrian324@gmail.com>
2024-09-13 10:32:12 +0700

    finishing snake game with high score feature

--- section 24/project 1/scoreboard.py
+++ section 24/project 1/scoreboard.py
@@ -1,6 +1,7 @@
 from turtle import Turtle
 ALIGNMENT = "center"
 FONT = ("Courier", 24, "normal")
+FILEPATH = "section 24/project 1/high_score.txt"
 
 
 class Scoreboard(Turtle):
@@ -8,6 +9,8 @@ class Scoreboard(Turtle):
     def __init__(self):
         super().__init__()
         self.score = 0
+        with open(FILEPATH) as high_score:
+            self.high_score = int(high_score.read())
         self.color("white")
         self.penup()
         self.goto(0, 270)
@@ -15,13 +18,21 @@ class Scoreboard(Turtle):
         self.update_scoreboard()
 
     def update_scoreboard(self):
-        self.write(f"Score: {self.score}", align=ALIGNMENT, font=FONT)
+        self.clear()
+        self.write(f"Score: {self.score} High Score: {self.high_score}", align=ALIGNMENT, font=FONT)
 
-    def game_over(self):
-        self.goto(0, 0)
-        self.write("GAME OVER", align=ALIGNMENT, font=FONT)
+    def reset(self):
+        if self.score > self.high_score:
+            self.high_score = self.score
+            with open(FILEPATH, mode='w') as high_score:
+                high_score.write(f"{self.high_score}")
+        self.score = 0
+        self.update_scoreboard()
+
+    # def game_over(self):
+    #     self.goto(0, 0)
+    #     self.write("GAME OVER", align=ALIGNMENT, font=FONT)
 
     def increase_score(self):
         self.score += 1
-        self.clear()
         self.update_scoreboard()
--- section 24/project 1/snake.py
+++ section 24/project 1/snake.py
@@ -34,6 +34,13 @@ class Snake:
             new_y = self.segments[seg_num - 1].ycor()
             self.segments[seg_num].goto(new_x, new_y)
         self.head.forward(MOVE_DISTANCE)
+    
+    def reset(self):
+        for segment in self.segments:
+            segment.goto(1000, 1000)
+        self.segments.clear()
+        self.create_snake()
+        self.head = self.segments[0]
 
     def up(self):
         if self.head.heading() != DOWN:
